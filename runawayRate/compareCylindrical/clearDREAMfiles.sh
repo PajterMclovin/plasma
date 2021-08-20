f22a252250e4639f1e4dbf543d4d7fac05f7c9e0
Peter Halldestam <pajter_mclovin@hotmail.com>
2021-08-20 12:33:50 +0200

    added shell script for quick removal of h5-files

---
+++ runawayRate/compareCylindrical/clearDREAMfiles.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+# Created by Peter Halldestam 19/8/21.
+#
+# clearDREAMFiles.sh: Removes all dream_settings and output files.
+#
+
+DREAM_SETTINGS_DIR="./dream_settings/"
+DREAM_OUTPUTS_DIR="./outputs/"
+
+if test -z "$(ls $DREAM_SETTINGS_DIR)" || test -z "$(ls $DREAM_OUTPUTS_DIR)"
+then
+    echo "No old files detected."
+else
+    echo "Old files detected!"
+    while true
+    do
+        read -p "Do you wish to delete them? (y/n)" ans
+        case $ans in
+            [Yy]* )
+                rm -v $DREAM_SETTINGS_DIR*
+                rm -v $DREAM_OUTPUTS_DIR*
+                exit;;
+            [Nn]* )
+                echo "Ok bye..."
+                exit;;
+            quit )
+                exit;;
+            * )
+                echo "Please answer y/n.";;
+        esac
+    done
+fi
