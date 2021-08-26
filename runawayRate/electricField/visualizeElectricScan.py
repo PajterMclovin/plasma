516215162ee727c12639f55969a1a32b1df62ba3
Hannes Bergström <hannesb98@gmail.com>
2021-08-26 16:50:59 +0200

    made it so that visualizeElectricScan distinguishes between cylindrical and toroidal data

--- runawayRate/electricField/visualizeElectricScan.py
+++ runawayRate/electricField/visualizeElectricScan.py
@@ -8,7 +8,7 @@ Visualizes DREAM output data for with respect to electric field strength.
 
 import matplotlib.pyplot as plt
 import numpy as np
-import sys, os
+import sys, os, glob
 
 dir = os.path.dirname(os.path.realpath(__file__))
 
@@ -23,28 +23,43 @@ from DREAM.DREAMOutput import DREAMOutput
 sys.path.append(os.path.join(dir, 'outputs'))
 outputDir = os.path.join(dir, 'outputs')
 
+def plotElectricScan(ax, files):
+   E = np.zeros(len(files))
+   runawayRate = np.zeros(len(files))
+        
+   for i in range(len(files)):
+      do = DREAMOutput(os.path.join(outputDir, files[i]))
+      E[i] = do.eqsys.E_field[0,0]
+      runawayRate[i] = do.other.fluid.runawayRate.data[-1,0]
+   
+   sorted_indices =  E.argsort()   
+   ax.plot(E[sorted_indices], runawayRate[sorted_indices])
+
+   return ax
+
+
+
 if __name__ == '__main__':
 
     fig, ax = plt.subplots()
-
-    if len(sys.argv) == 1:
-        dirList = [fp for fp in os.listdir(outputDir) if fp.endswith(".h5")]
-        dirList.sort()
-        E = np.zeros(len(dirList))
-        runawayRate = np.zeros(len(dirList))
-        
-        for i in range(len(dirList)):
-            fp = dirList[i]
-            do = DREAMOutput(os.path.join(outputDir, fp))
-            E[i] = do.eqsys.E_field[0,0]
-            runawayRate[i] = do.other.fluid.runawayRate.data[-1,17]
-        
-        sorted_indices =  E.argsort()   
-        ax.plot(E[sorted_indices], runawayRate[sorted_indices])
-                
+    
+    cylFiles = glob.glob(outputDir+'/*cyl*.h5')
+    torFiles = glob.glob(outputDir+'/*tor*.h5')
+    
+    if cylFiles and torFiles:
+       print('\nCylindrical and toroidal simulation data found, ploting both.\n')
+       plotElectricScan(ax, cylFiles)
+       plotElectricScan(ax, torFiles)
+       ax.legend(['Cylindrical', 'Toroidal'], title = 'Geometry')
+    elif cylFiles:
+       print('\nCylindrical measurement data found.\n')
+       plotElectricScan(ax, cylFiles)
+    elif torFiles:
+       print('\nToroidal measurement data found.\n')
+       plotElectricScan(ax, torFiles)
     else:
-        raise ValueError('Did not expect any arguments.')
-
+       raise Exception('\nNo measurement data found.\n')
+    
     # plot settings
     plt.ylabel(r'runaway rate [$s^{-1}m^{-3}$]')
     plt.xlabel('electric field [V/m]')
