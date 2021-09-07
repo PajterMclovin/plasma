687900aae39d17740732ddbc3414f37bebf9bc2f
Peter Halldestam <pajter_mclovin@hotmail.com>
2021-09-07 16:13:00 +0200

    clean up

---
+++ runawayRate/geometryScans/elongation/visualize.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+"""
+Created by Peter Halldestam 1/9/2021,
+modified by Peter Halldestam 7/9/2021.
+
+Visualizes DREAM output data for a range of maximum elongations.
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+import sys, os
+
+dir = os.path.dirname(os.path.realpath(__file__))
+
+# plot helper function import
+sys.path.append(os.path.join(dir, '../..'))
+from plotDREAM import plotRunawayRateMinorRadius
+from plotDREAM import plotFluxSurface
+
+# Settings object import
+sys.path.append(os.path.join(dir, '../../..'))
+from parameters import DREAM_PATH
+sys.path.append(DREAM_PATH)
+from DREAM.DREAMOutput import DREAMOutput
+
+sys.path.append(os.path.join(dir, 'outputs'))
+outputDir = os.path.join(dir, 'outputs')
+
+
+fig, (ax1, ax2) = plt.subplots(1,2)
+
+# visualize specified output
+if len(sys.argv) == 2:
+    ax1 = plotRunawayRate(sys.argv[1], ax=ax1)
+    ax2 = plotFluxSurface(ax=ax2)
+
+# visualize all outputs
+elif len(sys.argv) == 1:
+    for fp in os.listdir(outputDir):
+        if fp.endswith(".h5"):
+            outputFile = os.path.join(outputDir, fp)
+            do = DREAMOutput(outputFile)
+            maxElongation = fp[len('output'):-3]
+            label = f'{maxElongation:.6}'
+            ax1 = plotRunawayRateMinorRadius(do, ax=ax1, label=label)
+            ax2 = plotFluxSurface(kappa=float(maxElongation), ax=ax2, label=label)
+
+else:
+    raise ValueError('Expected at most one argument.')
+
+# plot settings
+ax1.legend(title=r'Elongation $\kappa$')
+ax1.set_ylabel(r'runaway rate $\gamma$ [m$^{-1}$]')
+ax1.set_xlabel(r'minor radius $r$ [m]')
+ax2.set_ylabel(r'height $z$[m]')
+ax2.set_xlabel(r'major radius $R$ [m]')
+ax2.yaxis.set_label_position('right')
+ax2.yaxis.tick_right()
+plt.show()
---
+++ runawayRate/geometryScans/triangularity/generateTriangularityScan.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Created by Peter Halldestam 1/9/21,
+modified by peter Halldestam 7/9/21.
+
+Generates DREAM settings files for a range of triangularity maxima (geometric
+parameter). Add any single command line argument when running this script to
+visualize the magnetic field.
+"""
+import sys, os
+import numpy as np
+
+dir = os.path.dirname(os.path.realpath(__file__))
+
+# Parameters import
+sys.path.append(os.path.join(dir, '../../..'))
+from parameters import MAX_TRIANGULARITY
+
+# helper function import
+sys.path.append(os.path.join(dir, '../..'))
+from configureDREAM import ConfigureDREAM
+
+# Scan parameters
+nScanValues = 3
+scanValues = np.linspace(-1, 1, nScanValues)
+
+if __name__ == "__main__":
+
+    for scanValue in scanValues:
+
+        ConfigureDREAM(include=['fluid/runawayRate', 'fluid/gammaDreicer'],
+                       maxTriangularity=scanValue, verbose=(len(sys.argv)==2),
+                       output=f'outputs/output{scanValue}.h5',
+                       save=f'settings/setting{scanValue}.h5')
